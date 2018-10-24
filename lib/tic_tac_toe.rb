8a5f6569f36da5f7c0cefe48ef0e9f0624b9b628
Lauren Piontek <piontekle@gmail.com>
2018-10-24 00:18:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,8 +21,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def move(board, index, player)
+  board[index] = player
 end
 
 def position_taken?(board, location)
@@ -38,7 +38,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    player = current_player(board)
+    move(board, index, player)
     display_board(board)
   else
     turn(board)
