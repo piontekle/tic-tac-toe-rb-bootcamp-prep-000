f1da75f83868f97dd4afc86b0c657e712ce3f856
Lauren Piontek <piontekle@gmail.com>
2018-10-24 00:16:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,24 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns.even?
+    return "X"
+  else
+    return "O"
+  end
+end
+
+
